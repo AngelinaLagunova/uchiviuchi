091649bc264f4f666ebfa06638883b0712417c02
Angelina Lagunova <lagunova_gelya@mail.ru>
2026-04-18 21:33:10 +1000

    fixed

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_URL = '/api/v1/basevocabs/';
+const API_URL = '/api/v1/basevocabs';
+
+// const API_URL = 'http://учивыучи.рф:8000/api/v1/basevocabs';
 // const API_URL = 'http://127.0.0.1:8000/api/v1/basevocabs/';
 
 
